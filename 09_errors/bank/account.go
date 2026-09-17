02505281cd45ca86e78707936c04b2fc95b46cf1
Landfrei <worker.land.8@gmail.com>
2026-09-17 12:47:27 +0300

    feat: add custom error UnzureichendesGuthabenFehler and handling with errors.As

--- 09_errors/bank/account.go
+++ 09_errors/bank/account.go
@@ -7,13 +7,26 @@ type Konto struct {
 	Guthaben float64 // das Guthaben - баланс
 }
 
-// das Geld - гроші
-// die Geldabhebung - зняття грошей
+// unzureichend - недостатній
+// das Guthaben - баланс
+// der Fehler - помилка
+//die UnyureichendesGuthabenFehler - помилка недостатнього балансу
+type UnzureichendesGuthabenFehler struct {
+	Guthaben float64
+	Betrag   float64
+}
+
+func (e *UnzureichendesGuthabenFehler) Error() string {
+	return fmt.Sprintf("nicht genug Guthaben auf dem Konto: %.2f gefordert, aber nur %.2f vorhanden", e.Betrag, e.Guthaben) // der Fehler - помилка, gefordert - вимагався, vorhanden - наявний
+}
 func (k *Konto) GeldAbheben(betrag float64) error { // der Betrag - сума
 	if betrag <= 0 {
 		return fmt.Errorf("Fehler: der Betrag muss größer als 0 sein") // der Fehler - помилка
 	} else if betrag > k.Guthaben {
-		return fmt.Errorf("Fehler: nicht genug Guthaben auf dem Konto") // der Fehler - помилка
+		return &UnzureichendesGuthabenFehler{
+			Guthaben: k.Guthaben,
+			Betrag:   betrag,
+		}
 	}
 	k.Guthaben -= betrag
 	return nil
--- 09_errors/main.go
+++ 09_errors/main.go
@@ -2,25 +2,23 @@ package main
 
 import (
 	"09_errors/bank"
+	"errors"
 	"fmt"
 )
 
 func main() {
 	konto := &bank.Konto{Guthaben: 500}
-	err := konto.GeldAbheben(-50)
+	err := konto.GeldAbheben(600)
 	if err != nil {
-		fmt.Println("Fehler:", err) // der Fehler - помилка
-	} else {
-		fmt.Println("Neues Guthaben:", konto.Guthaben)
-	}
-	err = konto.GeldAbheben(600)
-	if err != nil {
-		fmt.Println("Fehler:", err) // der Fehler - помилка
-	} else {
-		fmt.Println("Neues Guthaben:", konto.Guthaben)
-	}
-	err = konto.GeldAbheben(200)
-	if err == nil {
-		fmt.Println("Erfolg! Neuer Kontostand:", konto.Guthaben) // der Erfolg - успіх, der Kontostand - баланс рахунку
+		var fundsErr *bank.UnzureichendesGuthabenFehler
+		if errors.As(err, &fundsErr) {
+			fmt.Println("=== Спрацювала німецька кастомна помилка! ===")
+			fmt.Printf("Запитано (Betrag): %.2f EUR\n", fundsErr.Betrag)
+			fmt.Printf("Доступно (Guthaben): %.2f EUR\n", fundsErr.Guthaben)
+			fmt.Printf("Дефіцит: %.2f EUR\n", fundsErr.Betrag-fundsErr.Guthaben)
+		} else {
+			fmt.Println("=== Спрацювала стандартна помилка! ===")
+			fmt.Println(err)
+		}
 	}
 }
